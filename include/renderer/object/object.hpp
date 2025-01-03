0058e41325b0f052a41482ee25535ea455b94bf4
SeriousMeow <SeriousMeow@users.noreply.github.com>
2025-01-03 13:55:18 +0500

    Добавь 3D объект

---
+++ include/renderer/object/object.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @brief 3D объект
+ */
+
+#pragma once
+
+#include <vector>
+
+#include "renderer/data_structures/primitives.hpp"
+
+namespace renderer {
+
+/**
+ * @brief 3D объект - контейнер для графических примитивов
+ */
+class Object {
+public:
+    using Iterator = std::vector<Triangle>::iterator;
+    using ConstIterator = std::vector<Triangle>::const_iterator;
+
+    Iterator Begin();
+    Iterator End();
+
+    ConstIterator Begin() const;
+    ConstIterator End() const;
+
+private:
+    std::vector<Triangle> triangles_;
+};
+
+};  // namespace renderer
---
+++ source/renderer/object/object.cpp
@@ -0,0 +1,17 @@
+#include <renderer/object/object.hpp>
+
+renderer::Object::Iterator renderer::Object::Begin() {
+    return triangles_.begin();
+}
+
+renderer::Object::Iterator renderer::Object::End() {
+    return triangles_.end();
+}
+
+renderer::Object::ConstIterator renderer::Object::Begin() const {
+    return triangles_.begin();
+}
+
+renderer::Object::ConstIterator renderer::Object::End() const {
+    return triangles_.end();
+}
